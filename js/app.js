09bd938aa732d63159f71f21f196792302922af7
Thiago Ceolin <thiagoceolin.ti@gmail.com>
2025-05-31 18:09:00 -0300

    v1.0 comentada

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-function alterarStatus(numero){
-    let gameClicado = document.getElementById(`game-${numero}`);
-    let imagem = gameClicado.querySelector('.dashboard__item__img');
-    let botao = gameClicado.querySelector('.dashboard__item__button');
+function alterarStatus(numero){//função que altera o stauts
+    let gameClicado = document.getElementById(`game-${numero}`);//declara a variavel gameClicado como o html com o parâmetro recebido 
+    let imagem = gameClicado.querySelector('.dashboard__item__img');//declara a variavel imagem do jogo selecionado 
+    let botao = gameClicado.querySelector('.dashboard__item__button'); //declara a variavel como o botão do jogo selecionado
 
-    if(imagem.classList.contains("dashboard__item__img--rented")){
-        imagem.classList.remove("dashboard__item__img--rented");
-        botao.classList.remove("dashboard__item__button--return");
-        botao.textContent = 'Alugar';
+    if(imagem.classList.contains("dashboard__item__img--rented")){//se a variavel imagem contem na sua lista a classe rented
+        imagem.classList.remove("dashboard__item__img--rented");//remove essa classe da imagem
+        botao.classList.remove("dashboard__item__button--return");//remove essa classe do botão
+        botao.textContent = 'Alugar';//troca o texto do botão
      
-    } else{
-    imagem.classList.add("dashboard__item__img--rented");
-    botao.textContent = 'Devolver';
-    botao.classList.add("dashboard__item__button--return");
+    } else{//se não
+    imagem.classList.add("dashboard__item__img--rented");//adiciona a classe a imagem
+    botao.textContent = 'Devolver';//troca o texto do botão
+    botao.classList.add("dashboard__item__button--return");//adiciona a classe ao botão
     }
    
 }
